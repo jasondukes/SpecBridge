ad0f5f36d3a986b8f65441c002187af2a03f0248
Jason Dukes <mail@jasondukes.com>
2025-12-05 15:47:37 -0800

    Initial Commit

---
+++ TwitchStreamer/TwitchStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchStreamerApp.swift
+//  TwitchStreamer
+//
+//  Created by Jason Dukes on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TwitchStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
